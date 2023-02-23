67cf41fb5e943704f7ac4df1f4804ee6a73c61b7
Kemily <kemily.r.heleno@gmail.com>
2023-02-23 17:33:47 -0300

    Add files via upload

---
+++ Anotações.cpp
@@ -0,0 +1,21 @@
+ /*anotações*/
+strlen - O strlen conta os caracteres digitados e retorna um valor de tipo int;
+strupr - ;
+strlwr - 
+strlcpy - copia uma variavel para dentro de outra;
+strcat - concatena (anexa uma coisa na outra) ;
+puts - mostra dados armazenados em gets ambos possuem o S de string;
+gets - armazena dados da variavel em char que se torna sting;
+strcmp - compara strings;
+strcmpi - tambem compara strings mais diferente do outro ele nao faz distinçao entre letras maiusculas ou minusculas;
+putchar - mostra o resultado;
+char - é como se fosse um int ou um float mas com ele tambem podemos digitar letras;
+getch - serve de scanf e pode armazenar 1 caractere mas ele não é mostrado na tela e não precisa de enter;
+getche - ele é o mesmo que o getch com a diferença de que ele mostra o caractere na tela;
+getchar - este é diferente do getch e do getche pois ele mostra na tela e precisa do enter assim voce pode digitar quantos caracteres quiser mas só sera considerado o primeiro caractere;
+i%2==0 (verifica se a posicao é par)
+vet[i]%2==0(verifica se o elemento é par)
+// Para usar uma string é necessario acrescentar uma nova biblioteca #include<string.h>
+// O puts pode ser substituido por printf
+//o gets so pode ser substituido por scanf se for digitada uma unica palavra
+
