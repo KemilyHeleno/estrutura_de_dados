6a36af389897fcf1ba9192c7f2d6a8a3e067e606
Kemily <kemily.r.heleno@gmail.com>
2023-04-13 19:41:12 -0300

    prova de Estrutura de dados Lineares
    Quando tiver tempo, refaça a prova, vale a pena para reforçar seus estudos e o que você errou.
    Se ainda não fez a prova utilize-a para estudar e se preparar para a buxa futura.

---
+++ prova_estrutura_de_dados_lineares_13_04_2023.cpp
@@ -0,0 +1,52 @@
+/*1- Considere o seguinte contexto: Um programador deve criar uma estrutura de dados de pilha utilizando um vetor.
+A pilha deverá armazenar valores inteiros e conter funções para inserção (push) e remoção (pop).
+Para testar a correta implementação da pilha, o programador deverá realizar as seguintes operações:
+
+		* Inserir (push) o valor 10 na pilha
+		* Inserir (push) o valor 20 na pilha
+		* Inserir (push) o valor 30 na pilha
+		* Inserir (push) o valor 40 na pilha
+		* Remover (pop) um valor da pilha
+		* Remover (pop) um outro valor da pilha
+	
+Assumindo que a implementação da pilha está correta e que as operações push e pop não causam overflow ou underflow,
+qual será o estado final da pilha após a execução das operações mencionadas acima?*/
+
+
+
+/*2- Desenvolvimento de algoritmo para manipulação de listas com vetores
+Considere o seguinte cenário:
+Você foi contratado como desenvolvedor em uma empresa que precisa criar uma solução para armazenar
+e manipular informações sobre os produtos em estoque. A empresa decidiu usar listas com vetores para
+armazenar os dados dos produtos, como ID do produto, nome, quantidade e preço unitário.
+Escreva um algoritmo que implemente uma lista com vetores para armazenar os dados dos produtos e
+execute as seguintes funções:
+		
+		* Inserir novo produto na lista (com verificação de espaço disponível e sem permitir duplicação de ID)
+		* remover um produto da lista com base em seu ID
+		* Atualizar a quantidade e/ou o preço unitário de um produto existente
+		* Listar todos os produtos em estoque, exibindo seus dados (ID, nome, quantidade e preço unitário)
+		* Calcular e exibir o valor total em estoque, considerando a quantidade e o preço unitário de cada produto.
+		
+Você pode assumir que o vetor tem uma capacidade máxima de armazenamento (por exemplo, 100 produtos) e deve
+garantir que as operações sejam eficientes e não causem problemas de desempenho.
+Escreva seu algoritmo em pseudocódigo ou em uma linguagem de programação de sua escolha (de preferência C).*/
+
+
+
+
+/*#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+
+
+main(){
+	strncpy(nome_usuario[maximo], valor, 50);
+	int id[100][50];
+	int quant[100];
+	float valor[100];
+	
+	
+}*/
